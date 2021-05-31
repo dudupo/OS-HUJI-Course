1f9c7345c7c6cac7bcf05df6550cd2f6123b6634
Chaya Mushka *_* <chaya.lasry@mail.huji.ac.il>
2021-05-31 20:48:49 +0300

    first commit

--- ex3/MapReduceFramework.h
+++ ex3/MapReduceFramework.h
@@ -15,9 +15,14 @@ typedef struct {
 void emit2 (K2* key, V2* value, void* context);
 void emit3 (K3* key, V3* value, void* context);
 
+"""
+hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
+"""
+
 JobHandle startMapReduceJob(const MapReduceClient& client,
 	const InputVec& inputVec, OutputVec& outputVec,
 	int multiThreadLevel);
+	int chaya;
 
 void waitForJob(JobHandle job);
 void getJobState(JobHandle job, JobState* state);
