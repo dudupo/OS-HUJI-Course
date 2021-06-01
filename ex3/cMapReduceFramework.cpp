453542fcc5b48c80edf150403f44f50266b333cd
dudupo <dudupo51@gmail.com>
2021-06-01 17:02:05 +0300

    separate cpp files

---
+++ ex3/cMapReduceFramework.cpp
@@ -0,0 +1,9 @@
+#include "MapReduceFramework.h"
+
+
+void emit2 (K2* key, V2* value, void* context) {
+
+}
+void emit3 (K3* key, V3* value, void* context) {
+    
+}
---
+++ ex3/dMapReduceFramework.cpp
@@ -0,0 +1,20 @@
+
+
+#include "MapReduceFramework.h"
+
+JobHandle startMapReduceJob(const MapReduceClient& client,
+	const InputVec& inputVec, OutputVec& outputVec,
+	int multiThreadLevel) {
+
+}
+
+
+void waitForJob(JobHandle job) {
+
+}
+void getJobState(JobHandle job, JobState* state) {
+
+}
+void closeJobHandle(JobHandle job) { 
+    
+}
