7143b117f41b66b5edb141a0a67002f93c5beee3
dudupo <dudupo51@gmail.com>
2021-06-05 11:09:27 +0300

    estetic additions

--- ex3/dMapReduceFramework.cpp
+++ ex3/dMapReduceFramework.cpp
@@ -21,17 +21,32 @@
 #endif
 
 
-// absoulte position 
-pthread_t ** threads;
+void * map_reduce_call (void * context);
 
 // const MapReduceClient& client
 
 class GlobalEnv {
     public :
-    GlobalEnv(const InputVec& inputVec, OutputVec& outputVec, const MapReduceClient& client, int multiThreadLevel) : inputVec(inputVec),
-     outputVec(outputVec), client(client), map(),
-      intermediateVecs(), barrier(multiThreadLevel), _size( inputVec.size()),
-      mutex(PTHREAD_MUTEX_INITIALIZER) { }
+    GlobalEnv(const InputVec& inputVec, OutputVec& outputVec,
+     const MapReduceClient& client, int multiThreadLevel) : inputVec(inputVec),
+      outputVec(outputVec), client(client), map(),
+       intermediateVecs(), barrier(multiThreadLevel), _size( inputVec.size()),
+        mutex(PTHREAD_MUTEX_INITIALIZER) { 
+
+        // absoulte position 
+        threads = (pthread_t **) malloc( sizeof(pthread_t *) * multiThreadLevel ); 
+
+        // allocate memmory on the heap.
+        for ( int i = 0 ; i < multiThreadLevel; i++ ) {
+            threads[i] = (pthread_t *) malloc( sizeof(pthread_t )); 
+        }
+
+        for ( int i = 0 ; i < multiThreadLevel; i++ ) {
+            threads[i] = (pthread_t *) malloc( sizeof(pthread_t )); 
+            pthread_create( threads[i], NULL, &map_reduce_call, this );
+        }
+
+    }
     
     InputVec inputVec;
     OutputVec& outputVec;
@@ -39,20 +54,20 @@ class GlobalEnv {
     Barrier barrier; 
     stage_t stage = UNDEFINED_STAGE;
     
+    pthread_t ** threads;
     pthread_mutex_t mutex;
 
     // absoulte memmory.
     std::vector<IntermediateVec*> intermediateVecs; 
-
-
+    
     // points on items in intermediateVecs.
-    struct cmpByStringLength {
+    struct cmpK2 {
         bool operator()(const K2* a, const K2* b) const {
             return *a < *b;
         }
     };
 
-    std::map<K2* , IntermediateVec*, cmpByStringLength > map; 
+    std::map<K2* , IntermediateVec*, cmpK2> map; 
     
     float _size;
     
@@ -112,66 +127,54 @@ pthread_mutex_t mutex;
 // should executed only once.
 void shuffle()
 {
+    log("");
     globalEnv->_size = globalEnv->map.size();
 }
 
 
-void * map_reduce_call (void * args)
+void * map_reduce_call (void * context)
 {
-    void * context = NULL;
     
-    globalEnv->stage=MAP_STAGE;
-
-    globalEnv->lock();
+    GlobalEnv * _globalEnv = (GlobalEnv *) context;
+    _globalEnv->stage=MAP_STAGE;
+    _globalEnv->lock();
 
     // while there are still missions in the queue. 
-    while ( globalEnv->inputVec.size() > 0 ){
+    while ( _globalEnv->inputVec.size() > 0 ){
         // pop one and execute it.
-        InputPair pair = globalEnv->inputVec.back();
-        globalEnv->inputVec.pop_back();
-        globalEnv->unlock();
+        InputPair pair = _globalEnv->inputVec.back();
+        _globalEnv->inputVec.pop_back();
+        _globalEnv->unlock();
         
-        globalEnv->client.map(pair.first, pair.second, context);
-        globalEnv->lock();
+        _globalEnv->client.map(pair.first, pair.second, context);
+        _globalEnv->lock();
     }
-    globalEnv->unlock();
+    _globalEnv->unlock();
     
-    globalEnv->barrier.barrier();    globalEnv->stage=SHUFFLE_STAGE;
-
-    globalEnv->stage=SHUFFLE_STAGE;
+    _globalEnv->barrier.barrier();    
+    _globalEnv->stage=SHUFFLE_STAGE;
     shuffle();
-    globalEnv->stage=REDUCE_STAGE;
-    globalEnv->barrier.barrier();
+    _globalEnv->stage=REDUCE_STAGE;
+    _globalEnv->barrier.barrier();
     
-    globalEnv->lock();
-    while ( globalEnv->map.size() > 0 ){
-        globalEnv->unlock();
-        globalEnv->client.reduce(globalEnv->mappop(), context);        
-        globalEnv->lock();
+    _globalEnv->lock();
+    while ( _globalEnv->map.size() > 0 ){
+        _globalEnv->unlock();
+        _globalEnv->client.reduce(_globalEnv->mappop(), context);        
+        _globalEnv->lock();
     }
-    globalEnv->unlock();
+    _globalEnv->unlock();
 }
 
 
 JobHandle startMapReduceJob(const MapReduceClient& client,
 	const InputVec& inputVec, OutputVec& outputVec,
 	int multiThreadLevel) {    
-    threads = (pthread_t **) malloc( sizeof(pthread_t *) * multiThreadLevel ); 
-
-    // allocate memmory on the heap.
-    for ( int i = 0 ; i < multiThreadLevel; i++ ) {
-        threads[i] = (pthread_t *) malloc( sizeof(pthread_t )); 
-    }
-
     globalEnv = new GlobalEnv (inputVec, outputVec, client, multiThreadLevel);
+    
+    
 
-    for ( int i = 0 ; i < multiThreadLevel; i++ ) {
-        threads[i] = (pthread_t *) malloc( sizeof(pthread_t )); 
-        pthread_create( threads[i], NULL, &map_reduce_call, globalEnv );
-    }
-
-
-    return NULL;
+    return (void *) globalEnv;
 }
 
 
@@ -180,27 +183,30 @@ void waitForJob(JobHandle job) {
 }
 void getJobState(JobHandle job, JobState* state) {
 
+    ((GlobalEnv *) job)->lock();
+
     float p = 0;
-    switch (globalEnv->stage)
+    switch (((GlobalEnv *) job)->stage)
     {
     case UNDEFINED_STAGE:
         p = 0;
         break;
     case MAP_STAGE:
-        p = float( globalEnv->inputVec.size()/ globalEnv->_size);
+        p = float( ((GlobalEnv *) job)->inputVec.size()/ ((GlobalEnv *) job)->_size);
         break;
     case SHUFFLE_STAGE:
         p = 0.5;
         break;
     case REDUCE_STAGE:
-        p = float( globalEnv->map.size()/ globalEnv->_size);
+        p = float( ((GlobalEnv *) job)->map.size()/ ((GlobalEnv *) job)->_size);
         break;
 
     default:
         break;
     }
     state->percentage = 100 * (1 -p );
-    state->stage = globalEnv->stage;
+    state->stage = ((GlobalEnv *) job)->stage;
+    ((GlobalEnv *) job)->unlock();
 }
 void closeJobHandle(JobHandle job) { 
  
@@ -208,8 +214,8 @@ void closeJobHandle(JobHandle job) {
 
 
 void emit2 (K2* key, V2* value, void* context) {
-    globalEnv->emit2( key, value );
+    ((GlobalEnv *) context)->emit2( key, value );
 }
 void emit3 (K3* key, V3* value, void* context) {
-    globalEnv->emit3(key, value);
+    ((GlobalEnv *) context)->emit3(key, value);
 }
