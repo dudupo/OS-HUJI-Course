2f9cf34a4d286d407cd2ed0661b58ecdb85368e0
dudupo <dudupo51@gmail.com>
2021-06-21 15:59:16 +0300

    stack in infinty loop, should create new leaf

--- ex4/VirtualMemory.cpp
+++ ex4/VirtualMemory.cpp
@@ -1,9 +1,10 @@
 #include "VirtualMemory.h"
 #include "PhysicalMemory.h"
 
+#include <cstdio>
 
-typdef uint64_t physical_addr;
-typdef uint64_t temp_type;
+typedef uint64_t physical_addr;
+typedef uint64_t temp_type;
 
 int toDec(uint64_t index){
     int dec_value = 0;
@@ -18,10 +19,11 @@ int toDec(uint64_t index){
     return dec_value;
 }
 
-bool tableIsClear(){
+bool tableIsClear( uint64_t frameIndex ){
     word_t* val;
+
     for (uint64_t i = 0; i < PAGE_SIZE; ++i) {
-      PMread(frameIndex * PAGE_SIZE) + i, val);
+      PMread( (frameIndex * PAGE_SIZE) + i, val);
       if(val == 0){
         return false;
       }
@@ -34,8 +36,6 @@ temp_type NewFrame();
 
 physical_addr& getPAddr(temp_type page){
 
-
-
 }
 
 
@@ -44,28 +44,61 @@ void clearTable(uint64_t frameIndex) {
         PMwrite((frameIndex * PAGE_SIZE) + i, 0);
     }
 }
-
 void VMinitialize() {
-    clearTable(0);
+    //clearTable(0);
 }
 
+inline physical_addr convert(uint64_t virtualAddress, physical_addr addr )
+{
+    temp_type offset    = virtualAddress & (PAGE_SIZE-1);
+    temp_type page      = virtualAddress >> PAGE_SIZE;
+    return addr * PAGE_SIZE + offset;
+} 
+
+physical_addr search(uint64_t virtualAddress) {
+    physical_addr addr = 0;
+    for ( int i = 0 ; i < TABLES_DEPTH  ; i++ ) {
+
+        printf( "[DEBUG] %d\n", i);
+
+        PMread(
+            convert( virtualAddress, addr),
+             (word_t *) &addr);
+
+        if ( addr == 0 )
+            return addr;            
+        
+
+        virtualAddress >>= PAGE_SIZE;
+    }
+    return addr;
+}
 
 int VMread(uint64_t virtualAddress, word_t* value) {
-    temp_type offset = virtualAddress & (PAGE_SIZE-1);
-    temp_type page =  virtualAddress >> PAGE_SIZE;
-    physical_addr addr = getPAddr(page);
-    if(addr == 0){return 0;}
-    PMread(addr * PAGE_SIZE+offset, value);
+    physical_addr val = 0 , preval = 0;
+    for  ( ; val  == 0 ; val =  search(virtualAddress) )  {
+            
+        // create, or evicate. 
+    }
+
+    // last step 
+    PMread (
+         convert( 0, val),
+            (word_t *) value);
+
     return 1;
 }
 
-
 int VMwrite(uint64_t virtualAddress, word_t value) {
-    temp_type offset = virtualAddress & (PAGE_SIZE-1);
-    temp_type page =  virtualAddress >> PAGE_SIZE;
-    physical_addr addr = getPAddr(page);
-    if(addr == 0){return 0;}
-    PMwrite(addr * PAGE_SIZE+offset, value);
+    
+    physical_addr val = 0;
+    for  (  ; val  == 0 ; val =  search(virtualAddress) )  {
+        // evicate 
+    }    
+
+    // last step 
+    PMwrite(
+        convert( 0 , val),  value);
     return 1;
 }
 
---
+++ ex4/compile.sh
@@ -0,0 +1,1 @@
+g++ SimpleTest.cpp -L. -l:libMemmory.a  -o test  --std=c++11
