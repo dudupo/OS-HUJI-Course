39cf69a0392fd15f9fbf95207d436002c539c8a3
lasrychaya <chaya.lasry@mail.huji.ac.il>
2021-06-21 13:25:03 +0300

    slight changes

--- ex4/VirtualMemory.cpp
+++ ex4/VirtualMemory.cpp
@@ -4,17 +4,6 @@
 
 typdef uint64_t physical_addr;
 typdef uint64_t temp_type;
-typedef struct VirtualMem{
-    temp_type offset;
-    temp_type page;
-
-}VirtualMem;
-
-
-void parseVAddr(uint64_t& virtualAddress,VirtualMem& VMem ){
-    VMem.offset = virtualAddress & (PAGE_SIZE-1);
-    VMem.page =  virtualAddress >> PAGE_SIZE;
-}
 
 int toDec(uint64_t index){
     int dec_value = 0;
@@ -62,19 +51,21 @@ void VMinitialize() {
 
 
 int VMread(uint64_t virtualAddress, word_t* value) {
-    physical_addr addr = getPAddr(virtualAddress);
+    temp_type offset = virtualAddress & (PAGE_SIZE-1);
+    temp_type page =  virtualAddress >> PAGE_SIZE;
+    physical_addr addr = getPAddr(page);
     if(addr == 0){return 0;}
-    PMread(addr, value);
+    PMread(addr * PAGE_SIZE+offset, value);
     return 1;
 }
 
 
 int VMwrite(uint64_t virtualAddress, word_t value) {
-    VirtualMem mem;
-    parseVAddr(VMem, mem);
-    physical_addr addr = getPAddr(mem.page);
+    temp_type offset = virtualAddress & (PAGE_SIZE-1);
+    temp_type page =  virtualAddress >> PAGE_SIZE;
+    physical_addr addr = getPAddr(page);
     if(addr == 0){return 0;}
-    PMwrite(addr * PAGE_SIZE+mem.offset, value);
+    PMwrite(addr * PAGE_SIZE+offset, value);
     return 1;
 }
 
