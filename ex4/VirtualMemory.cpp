f1c84d0efdd179300604569c2bb0e6415afd4ab9
dudupo <dudupo51@gmail.com>
2021-06-24 21:03:31 +0300

    pass Once test

--- ex4/VirtualMemory.cpp
+++ ex4/VirtualMemory.cpp
@@ -2,7 +2,7 @@
 #include "PhysicalMemory.h"
 
 #include <cstdio>
-
+#include <iostream>
 
 
 #define DEBUG
@@ -58,14 +58,35 @@ void VMinitialize() {
     clearTable(0);
 }
 
+struct Path {
+    physical_addr paths[TABLES_DEPTH+1]; 
+};
 
-physical_addr convert(uint64_t virtualAddress, physical_addr addr )
+
+#include <cmath>
+
+struct Path get_path(uint64_t virtualAddress) {
+
+    log();
+    struct Path ret;
+    // ret.paths[TABLES_DEPTH]  = virtualAddress & WORD_WIDTH;
+    // virtualAddress >> WORD_WIDTH;
+    for (int i = 0 ; i <= TABLES_DEPTH; virtualAddress >>= OFFSET_WIDTH ) {
+        std::cout << "virtualAddress : " <<  virtualAddress << "\n";
+        ret.paths[TABLES_DEPTH - i] = virtualAddress & (PAGE_SIZE-1);     
+        i++;
+    }
+    for (int i = 0; i <= TABLES_DEPTH; ++i){
+        std::cout << ret.paths[i] << "\n";
+    }
+    return ret;
+}
+
+physical_addr convert( physical_addr offset, physical_addr addr)
 {
-    temp_type offset    = virtualAddress % PAGE_SIZE ;
-    temp_type page      = virtualAddress >> PAGE_SIZE;
     log()
-    printf("frame: %d \n", addr);
-    printf("offset: %lu \n", offset);
+    // printf("frame: %d \n", addr);
+    // printf("offset: %lu \n", offset);
     printf("total: %llu \n", addr * PAGE_SIZE + offset);
     return addr * PAGE_SIZE + offset;
 }
@@ -81,39 +102,35 @@ struct AddressState search(uint64_t virtualAddress) {
     struct AddressState ret = { 0, 0};
     word_t qal = 0;
     uint64_t temp_add = virtualAddress;
+    
+    struct Path offsets = get_path( virtualAddress );
+    
     for ( int i = 0 ; i < TABLES_DEPTH; i++ ) {
-
-        // printf( "[DEBUG] depth %d, PAGE_SIZE : %d , TABLES_DEPTH : %d \n", i, PAGE_SIZE, TABLES_DEPTH);
-        // printf( "[DEBUG] val.addr : %d, val.nextaddr  : %d \n", ret.addr, ret.nextaddr);
-        temp_add = (virtualAddress >> (OFFSET_WIDTH * (TABLES_DEPTH - i - 1)));
-        PMread(convert(temp_add,ret.addr), &qal );
-
-        ret.nextaddr= qal;
-        if ( ret.nextaddr == 0 ) {
+        
+        PMread(convert(offsets.paths[i],ret.addr), &qal );        
+        if ( qal == 0 ) {
             log()
             // pathch, should remove.
-            ret.addr = convert( temp_add, ret.addr);
-            printf("%d \n", ret.addr);
+            ret.addr = convert(offsets.paths[i],ret.addr);
             ret.nextaddr = -1;
             return ret;
         }
-//        virtualAddress /= PAGE_SIZE;
-
-
-        ret.addr = ret.nextaddr;
+        else{
+            ret.addr = qal;
+        }
     }
-    // printf( "[DEBUG] val.addr : %d, val.nextaddr  : %d \n", ret.addr, ret.nextaddr);
+    ret.nextaddr = qal;
     return ret;
 }
 
 
-#define BFS
+// #define BFS
 #ifdef BFS
 #include <fstream>
 #include <vector>
 #include <utility>
 #include <iostream>
-void print_BFS(std::vector< std::pair<physical_addr,  physical_addr>> queue, std::ofstream & _file ) {
+void print_BFS(std::vector< std::pair<physical_addr,  physical_addr>> & queue, std::ofstream & _file ) {
 
     if (queue.empty())
         return;
@@ -123,16 +140,16 @@ void print_BFS(std::vector< std::pair<physical_addr,  physical_addr>> queue, std
     _file << ppair.first << " " <<  ppair.second << "\n";
     word_t temp_addr = 0;
     
-    for (uint64_t i = 0; i < PAGE_SIZE ; ++i) {
-         
-	 if ( (ppair.second * PAGE_SIZE + i) < RAM_SIZE ){
-         	PMread(ppair.second * PAGE_SIZE + i, &temp_addr);
-	 	if (temp_addr != 0) {
-            		queue.push_back(std::pair<physical_addr, physical_addr>( ppair.second * PAGE_SIZE + i, temp_addr));
-         	}
-	 }
-         
+    for (uint64_t i = 0; i < PAGE_SIZE ; ++i) {        
+        if ( (ppair.second * PAGE_SIZE + i) < RAM_SIZE ){
+            PMread(ppair.second * PAGE_SIZE + i, &temp_addr);
+            if (temp_addr != 0) {
+                    queue.push_back(std::pair<physical_addr, physical_addr>(
+                        ppair.second * PAGE_SIZE + i, temp_addr));
+            }
+        }         
     }
+
     print_BFS(queue, _file );
 }
 
@@ -187,25 +204,23 @@ int NewNode ( physical_addr addr, uint64_t frame ) {
 
 struct AddressState  getPAddr(uint64_t virtualAddress){
     log()
-
-    log()
-    virtualAddress = virtualAddress >> OFFSET_WIDTH;
+    // virtualAddress = virtualAddress >> OFFSET_WIDTH;
     struct AddressState val = {0,0};
-    val.nextaddr =-1;
+    val = search(virtualAddress);
     for  ( ; val.nextaddr  == -1 ; val = search(virtualAddress) )  {
 
         physical_addr emptyframe = -1;
         // after initialize new node, the new should point to it's perent
-        PMwrite(val.addr, val.addr / PAGE_SIZE );
         findFreeTable( 0, 0 ,emptyframe  );
         if ( emptyframe == -1 ) {
-            PMwrite(val.addr, 0 );
+            // PMwrite(val.addr, 0 );
             emptyframe = getmax_frame_index_DFS(0, 0 );
             if (emptyframe >= NUM_FRAMES) {
                 //evict!
             }
         }
         NewNode(val.addr, emptyframe );
+        // PMwrite(val.addr, val.addr / PAGE_SIZE );
     }
 
     return val;
@@ -218,7 +233,7 @@ int VMread(uint64_t virtualAddress, word_t* value) {
     printf("[DEBUG] VMread addr:  %d,\n", val.addr);
     // last step
     PMread (
-         convert( 0, val.nextaddr),
+         convert( virtualAddress &  (PAGE_SIZE-1), val.addr),
             (word_t *) value);
 
     return 1;
@@ -229,7 +244,7 @@ int VMwrite(uint64_t virtualAddress, word_t value) {
 
     // last step
     PMwrite(
-        convert( 0 , val.nextaddr),  value);
+        convert( virtualAddress &  (PAGE_SIZE-1) , val.addr),  value);
     printf("[DEBUG] write addr:  %d, the val: %d\n", val.addr, value);
 
 
